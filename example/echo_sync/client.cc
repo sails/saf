74fb8e6d3cab7206efc07cbd8663d59c3ec5d716
sailsxu <sailsxu@xu-2.local>
2014-12-09 10:19:14 +0800

    update report info

--- example/echo_sync/client.cc
+++ example/echo_sync/client.cc
@@ -30,7 +30,7 @@ void test_fun(RpcChannelImp &channel, RpcControllerImp &controller) {
     
     stub.add(&controller, &request, &response, callback);
     
-//        std::cout << response.DebugString() << std::endl;
+ //       std::cout << response.DebugString() << std::endl;
 }
 
 
