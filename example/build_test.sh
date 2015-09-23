01c7c42662ef2990511febbacbade4f07cf41bdc
sails <sailsxu@gmail.com>
2015-09-23 16:52:36 +0800

    update

--- example/build_test.sh
+++ example/build_test.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+#生成对应的服务类
+if [ ! -f "./echo_sync/addressbook.pb.cc" ]; then
+    cd ./echo_sync/; protoc --cpp_out=./ addressbook.proto; cd ../;
+fi
+if [ ! -f "./Login/login.pb.cc" ]; then
+    cd ./Login; protoc --cpp_out=./ login.proto; cd ../;
+fi
+if [ ! -f "./rank/rank.pb.cc" ]; then
+    cd ./rank; protoc --cpp_out=./ rank.proto; cd ../;
+fi
+if [ ! -f "./rpcping/ping.pb.cc" ]; then
+    cd ./rpcping; protoc --cpp_out=./ ping.proto; cd ../;
+fi
+
 cd echo_sync
 make
 make service_test
