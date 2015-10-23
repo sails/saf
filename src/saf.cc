edf768678d0826002425a69b2cfe58adff2e3a1b
sails <sailsxu@gmail.com>
2015-10-23 16:41:30 +0800

    update

--- src/saf.cc
+++ src/saf.cc
@@ -47,7 +47,7 @@ void sails_init(int, char **) {
 
   // 初始化server
   server.Init(config.get_listen_port(), config.get_net_thread(), 10,
-              config.get_handle_thread(), false, 2);
+              config.get_handle_thread());
 
   monitor = new sails::Monitor(&server, config.get_monitor_port());
   monitor->Run();
